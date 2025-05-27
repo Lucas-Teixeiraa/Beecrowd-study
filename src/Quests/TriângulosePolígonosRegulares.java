d30c99c2f392ab3c2a631a14ba5ab5be6a8dc13c
Lucas <lteixeira507@gmail.com>
2025-05-27 15:05:42 -0400

    2058 beecrowd

--- src/Quests/TriângulosePolígonosRegulares.java
+++ src/Quests/TriângulosePolígonosRegulares.java
@@ -2,7 +2,7 @@ package Quests;
 
 import java.util.Scanner;
 
-//BeeCrowd - 1933 - Lucas Teixeira
+//BeeCrowd - 2058 - Lucas Teixeira
 public class TriângulosePolígonosRegulares {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
